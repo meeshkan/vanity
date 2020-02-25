c0d0ca15e5d5b2c013ef671bc11d121dd71af429
k4m4 <nikolaskam@gmail.com>
2020-02-25 09:27:13 -0500

    Split workflow in two

--- backend/workers/workflows/triggerMetricsIngestion.js
+++ backend/workers/workflows/triggerMetricsIngestion.js
@@ -1,5 +1,5 @@
 const { QUEUE_CRON } = require('../../config');
 
 module.exports.handle = function*({ user }) {
-    this.schedule(QUEUE_CRON.EMAIL).task('sendEmail', user);
+    this.schedule(QUEUE_CRON.METRICS).task('ingestMetrics', user);
 };
