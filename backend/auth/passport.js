879899250abf26ff2d14aa6c3d75945c02a653b7
Mike Solomon <mike@meeshkan.com>
2020-02-25 16:49:00 +0200

    Adds test

--- backend/auth/passport.js
+++ backend/auth/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
-const { Strategy } = require('passport-github');
+const GithubStrategy = require('passport-github').Strategy;
+const MockStrategy = require('./mock-strategy').Strategy;
 const _ = require('lodash');
 const { User } = require('../models');
 const { fetchUserRepos } = require('../utils/github');
@@ -21,73 +22,73 @@ const filterUser = user => {
 
 const containSameElements = (x, y) => _.isEqual(_.sortBy(x), _.sortBy(y));
 
-passport.use(
-	new (process.env.NODE_ENV == 'test' ? require('passport-mocked').Strategy : Strategy)(
-		PASSPORT_OPTIONS,
-		async (accessToken, refreshToken, profile, done) => {
-			const { username, emails, photos } = profile;
-			const email = getPrimaryEmail(emails);
-			let latestRepos = await fetchUserRepos(username, accessToken);
-
-			User.upsert(
-				{
-					username,
-					email,
-					token: accessToken,
-					avatar: photos[0].value,
-				},
-				{
-					returning: true,
-					fields: ['username', 'email', 'token', 'avatar'],
-				}
-			)
-				.then(([user, created]) => {
-					user = user.get({ plain: true });
-
-					// TODO: move this to a function & re-call it with a `sync` button in /preferences
-					const latestReposNames = latestRepos.map(repo => repo.name);
-					if (!(user.repos && containSameElements(latestReposNames, user.repos.map(repo => repo.name)))) {
-						latestRepos = latestRepos.map(repo => {
-							repo.selected = !repo.fork;
-							return repo;
-						});
-
-						if (user.repos === null) {
-							user.repos = latestRepos;
-						} else {
-							latestRepos.forEach(repo => {
-								if (!(repo.name in user.repos.map(repo => repo.name))) {
-									user.repos.push(repo);
-								}
-							});
-						}
-
-						User.update(
-							{
-								repos: user.repos,
-							},
-							{
-								where: {
-									id: user.id,
-								},
-								fields: ['repos'],
-							},
-						); // TODO: handle response
-					}
-
-					if (created) {
-						ingestMetricsJob(user);
-						sendSampleEmailJob(user);
-						sendEmailJob(user);
-					}
-
-					return done(null, filterUser(user));
-				})
-				.catch(error => {
-					return done(error);
-				});
+const strategyCallback = async (accessToken, refreshToken, profile, done) => {
+	const { username, emails, photos } = profile;
+	const email = getPrimaryEmail(emails);
+	let latestRepos = await fetchUserRepos(username, accessToken);
+	User.upsert(
+		{
+			username,
+			email,
+			token: accessToken,
+			avatar: photos[0].value,
 		},
+		{
+			returning: true,
+			fields: ['username', 'email', 'token', 'avatar'],
+		}
 	)
+		.then(([user, created]) => {
+			user = user.get({ plain: true });
+
+			// TODO: move this to a function & re-call it with a `sync` button in /preferences
+			const latestReposNames = latestRepos.map(repo => repo.name);
+			if (!(user.repos && containSameElements(latestReposNames, user.repos.map(repo => repo.name)))) {
+				latestRepos = latestRepos.map(repo => {
+					repo.selected = !repo.fork;
+					return repo;
+				});
+
+				if (user.repos === null) {
+					user.repos = latestRepos;
+				} else {
+					latestRepos.forEach(repo => {
+						if (!(repo.name in user.repos.map(repo => repo.name))) {
+							user.repos.push(repo);
+						}
+					});
+				}
+
+				User.update(
+					{
+						repos: user.repos,
+					},
+					{
+						where: {
+							id: user.id,
+						},
+						fields: ['repos'],
+					},
+				); // TODO: handle response
+			}
+
+			if (created) {
+				ingestMetricsJob(user);
+				sendSampleEmailJob(user);
+				sendEmailJob(user);
+			}
+
+			return done(null, filterUser(user));
+		})
+		.catch(error => {
+			return done(error);
+		});
+}
+
+passport.use(
+	process.env.NODE_ENV == 'test' ? new MockStrategy('github', strategyCallback) : new GithubStrategy(
+		PASSPORT_OPTIONS,
+		strategyCallback)
 );
 
 passport.serializeUser((user, done) => done(null, user));
