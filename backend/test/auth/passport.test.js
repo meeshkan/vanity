c03bf805c3e8713f60060b92dd160b13e6b127a7
k4m4 <nikolaskam@gmail.com>
2020-02-28 14:41:52 -0500

    Add error assertion to passport callback test

--- backend/test/auth/passport.test.js
+++ backend/test/auth/passport.test.js
@@ -3,7 +3,8 @@ const { strategyCallback } = require('../../auth/passport');
 const { GH_PROFILE } = require('../../auth/__fixtures__');
 
 test.cb('passport strategy callback', t => {
-	strategyCallback(undefined, undefined, GH_PROFILE, (_, user) => {
+	strategyCallback(undefined, undefined, GH_PROFILE, (error, user) => {
+        t.is(error, null);
 		t.is(user.username, GH_PROFILE.username);
 		t.is(user.avatar, GH_PROFILE.photos[0].value);
 		t.end();
