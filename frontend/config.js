c03df1865cd98e29a3099891a5cf9ba023d74cde
k4m4 <nikolaskam@gmail.com>
2020-03-13 13:40:25 -0400

    Use absolute path to .env file

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,6 @@
+const { join } = require('path');
 const dotenv = require('dotenv');
-dotenv.config({ path: '../.env' });
+dotenv.config({ path: join(__dirname, '../.env') });
 
 const {
 	VANITY_URI,
