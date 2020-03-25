3dcb0a732af636372dea0fc571ad14c0ff262289
k4m4 <nikolaskam@gmail.com>
2020-03-26 01:48:39 +0200

    Rename "Main" page story as "Home" page

---
+++ frontend/stories/home.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import HomePage from '../pages';
+
+export default { title: 'Home' };
+
+export const homePage = () => <HomePage />;
--- frontend/stories/main-page.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import MainPage from '../pages';
-
-export default { title: 'Main page' };
-
-export const wholePage = () => <MainPage />;
