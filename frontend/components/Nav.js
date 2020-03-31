38166d8daaf096657cb3471f33e813b90d4c7239
k4m4 <nikolaskam@gmail.com>
2020-03-31 17:37:24 +0300

    Replace "GitHub" with "Source" on navbar

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -66,7 +66,7 @@ export default function Nav() {
 							rel='noopener noreferrer'
 							title='source'
 						>
-							GitHub
+							Source
 						</a>
 						{avatarURL ? (
 							<Link href='/preferences'>
