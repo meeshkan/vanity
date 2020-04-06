084055af394c316afc301ec64ec7519dbdab1990
k4m4 <nikolaskam@gmail.com>
2020-04-06 17:35:24 +0300

    Remove Intercom chat

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -76,46 +76,6 @@ class VanityDoc extends Document {
 							`
 						}}
 					/>
-					<script
-						dangerouslySetInnerHTML={{
-							__html: `
-								window.intercomSettings = {
-									app_id: "nou4ik17"
-								};
-								(function() {
-									var w = window;
-									var ic = w.Intercom;
-									if (typeof ic === "function") {
-										ic('reattach_activator');
-										ic('update', w.intercomSettings);
-									} else {
-										var d = document;
-										var i = function() {
-											i.c(arguments);
-										};
-										i.q = [];
-										i.c = function(args) {
-											i.q.push(args);
-										};
-										w.Intercom = i;
-										var l = function() {
-											var s = d.createElement('script');
-											s.type = 'text/javascript';
-											s.async = true;
-											s.src = 'https://widget.intercom.io/widget/nou4ik17';
-											var x = d.getElementsByTagName('script')[0];
-											x.parentNode.insertBefore(s, x);
-										};
-										if (w.attachEvent) {
-											w.attachEvent('onload', l);
-										} else {
-											w.addEventListener('load', l, false);
-										}
-									}
-								})();
-							`
-						}}
-					/>
 				</Head>
 				<body>
 					<Main />
