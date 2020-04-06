94fff2021b8755250c70b63b03546ba1250df0a9
k4m4 <nikolaskam@gmail.com>
2020-04-06 16:49:01 +0300

    Create unsubscribe storybook

---
+++ frontend/stories/unsubscribe.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Unsubscribe } from '../pages/unsubscribe';
+
+export default { title: 'Unsubscribe' };
+
+export const successfulUnsubscription = () => (
+	<Unsubscribe
+		email='foo@bar.com'
+		message={undefined}
+	/>
+);
+
+export const unsuccessfulUnsubscription = () => (
+	<Unsubscribe
+		email={undefined}
+		message='Unsubscription token is invalid'
+	/>
+);
