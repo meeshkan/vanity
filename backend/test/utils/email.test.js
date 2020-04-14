129f700288ac7f89572b420479bf0ae0b0804f3c
k4m4 <nikolaskam@gmail.com>
2020-04-14 20:13:45 +0300

    Create weekly email sending test

--- backend/test/utils/email.test.js
+++ backend/test/utils/email.test.js
@@ -1,11 +1,13 @@
 const test = require('ava');
+const sinon = require('sinon');
+const SGMail = require('@sendgrid/mail');
 const {
 	createWeeklyEmail,
 	createSampleEmail,
 	sendSample,
 	send,
 } = require('../../utils/email');
-const { USER, WEEKLY_METRICS, SAMPLE_METRICS } = require('../__fixtures__');
+const { USER, WEEKLY_METRICS, SAMPLE_METRICS, SENDGRID_SUCCESS } = require('../__fixtures__');
 const { EMAIL_CONFIG } = require('../../config');
 
 const HTML_REGEX = /\s?<!doctype html>|(<html\b[^>]*>|<body\b[^>]*>|<x-[^>]+>)+/i;
@@ -58,4 +60,21 @@ test('generates sample email for empty metrics', async t => {
 	t.is(email.subject, EMAIL_CONFIG.sampleSubject);
 	t.regex(email.html, HTML_REGEX);
 	t.regex(email.html, /It seems like you don&apos;t have any repos yet/);
+});
+
+test('sends a weekly email', async t => {
+	const SGMailSend = sinon.stub(SGMail, 'send');
+	SGMailSend.returns(Promise.resolve(SENDGRID_SUCCESS));
+
+	const data = {
+		user: USER,
+		metrics: WEEKLY_METRICS,
+	};
+
+	const response = await send(data);
+	t.true(SGMailSend.calledOnce);
+	sinon.assert.calledWith(SGMailSend, createWeeklyEmail(data));
+	t.deepEqual(response, SENDGRID_SUCCESS);
+
+	SGMailSend.restore();
 });
