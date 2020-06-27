363bdf8e4db6c4c582f99fca911c68afbcce6491
k4m4 <nikolaskam@gmail.com>
2020-06-28 01:57:28 +0300

    Use console logging transport in all environments

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,7 +1,7 @@
 const { createLogger, format, transports } = require('winston');
 const Sentry = require('winston-transport-sentry-node').default;
 const { combine, json, timestamp, simple } = format;
-const { NODE_ENV, SENTRY_DSN } = require('../config');
+const { SENTRY_DSN } = require('../config');
 
 const logger = createLogger({
 	level: 'info',
@@ -12,20 +12,17 @@ const logger = createLogger({
 				dsn: SENTRY_DSN,
 			},
 			level: 'info'
+		}),
+		new transports.Console({
+			format: combine(
+				timestamp({
+					format: 'YYYY-MM-DD HH:mm:ss'
+				}),
+				simple()
+			),
 		})
 	],
 	exitOnError: false,
 });
-
-if (NODE_ENV === 'development') {
-	logger.add(new transports.Console({
-		format: combine(
-			timestamp({
-				format: 'YYYY-MM-DD HH:mm:ss'
-			}),
-			simple()
-		),
-	}));
-}
 
 module.exports = logger;
