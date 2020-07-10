0045a7b51472e5402149d3ca08a384ad3acf0e4e
k4m4 <nikolaskam@gmail.com>
2020-07-10 21:37:05 +0300

    Display 'edit profile' button in home page when logged in

--- frontend/components/Landing.js
+++ frontend/components/Landing.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
+import Cookies from 'js-cookie';
 import Footer from './Footer';
 import GitHubIcon from './GitHubIcon';
 
@@ -17,35 +18,64 @@ Page.propTypes = {
 	]).isRequired,
 };
 
-const Landing = () => (
-	<div>
-		<div className='landing-container'>
-			<Page>
-				<div className='absolute w-100 vh-100'>
-					<article className='vh-100 dt w-100'>
-						<div className='dtc v-mid tc white ph3 ph4-l'>
-							<h2 className='f2 f1-ns fw6 tc ttu f-subheadline-l mt0 mb3'>
-								V A N I T Y
-							</h2>
-							<p className='i f5 tracked-mega-l ph4 lh-copy'>weekly metrics for your GitHub repos</p>
-							<div className='f6 f5-l pv3 v-mid'>
-								<h3>start receiving your metrics</h3>
-								<p>
-									<Link href='/auth/github'>
-										<a className='white dim no-underline dt center'>
-											<span className='v-mid dt mb3'>Login with GitHub</span>
-											<GitHubIcon />
-										</a>
-									</Link>
-								</p>
+const Landing = () => {
+	const [username, setUsername] = useState('');
+
+	useEffect(() => {
+		const githubUserCookie = Cookies.get('github-user');
+		if (githubUserCookie) {
+			const { username } = JSON.parse(
+				decodeURIComponent(githubUserCookie)
+			);
+			setUsername(username);
+		}
+	}, []);
+
+	return (
+		<div>
+			<div className='landing-container'>
+				<Page>
+					<div className='absolute w-100 vh-100'>
+						<article className='vh-100 dt w-100'>
+							<div className='dtc v-mid tc white ph3 ph4-l'>
+								<h2 className='f2 f1-ns fw6 tc ttu f-subheadline-l mt0 mb3'>
+									V A N I T Y
+								</h2>
+								<p className='i f5 tracked-mega-l ph4 lh-copy'>weekly metrics for your GitHub repos</p>
+								<div className='f6 f5-l pv3 v-mid'>
+									{username ? (
+										<>
+											<h3>hey, {username}</h3>
+											<p>
+												<Link href='/preferences'>
+													<a className='link f5 ph3 pv2 dib black ba bw1 b--white br2 bg-white bg-animate hover-bg-transparent hover-white'>
+														edit your preferences
+													</a>
+												</Link>
+											</p>
+										</>
+									) : (
+										<>
+											<h3>start receiving your metrics</h3>
+											<p>
+												<Link href='/auth/github'>
+													<a className='white dim no-underline dt center'>
+														<span className='v-mid dt mb3'>Login with GitHub</span>
+														<GitHubIcon />
+													</a>
+												</Link>
+											</p>
+										</>
+									)}
+								</div>
 							</div>
-						</div>
-					</article>
-				</div>
-			</Page>
-			<Footer />
+						</article>
+					</div>
+				</Page>
+				<Footer />
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default Landing;
