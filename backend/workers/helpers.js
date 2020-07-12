3c6526383b90a555ce0f281135b9ae6fd0d9a964
k4m4 <nikolaskam@gmail.com>
2020-07-12 18:51:13 +0300

    Modify getRepeatableJobsByID to also return 'active' and 'waiting' jobs

--- backend/workers/helpers.js
+++ backend/workers/helpers.js
@@ -1,8 +1,8 @@
 const { ingestMetrics, sendEmail } = require('./queues');
 
 const getRepeatableJobsByID = async id => {
-	const ingestMetricsJobs = await ingestMetrics.getJobs(['delayed']);
-	const sendEmailJobs = await sendEmail.getJobs(['delayed']);
+	const ingestMetricsJobs = await ingestMetrics.getJobs(['delayed', 'waiting', 'active']);
+	const sendEmailJobs = await sendEmail.getJobs(['delayed', 'waiting', 'active']);
 
 	return {
 		ingestMetrics: await ingestMetricsJobs.find(delayedJob => delayedJob.opts.repeat.jobId === id),
