7331bdd30f76a332a101030a7c35bc4ffedd4372
k4m4 <nikolaskam@gmail.com>
2020-07-13 16:31:50 +0300

    Create matchesJobId() to avoid repetition

--- backend/workers/helpers.js
+++ backend/workers/helpers.js
@@ -4,9 +4,10 @@ const getRepeatableJobsByID = async id => {
 	const ingestMetricsJobs = await ingestMetrics.getJobs(['delayed', 'waiting', 'active']);
 	const sendEmailJobs = await sendEmail.getJobs(['delayed', 'waiting', 'active']);
 
+	const matchesJobId = delayedJob => delayedJob.opts.repeat.jobId === id;
 	return {
-		ingestMetrics: await ingestMetricsJobs.find(delayedJob => delayedJob.opts.repeat.jobId === id),
-		sendEmail: await sendEmailJobs.find(delayedJob => delayedJob.opts.repeat.jobId === id),
+		ingestMetrics: await ingestMetricsJobs.find(delayedJob => matchesJobId(delayedJob)),
+		sendEmail: await sendEmailJobs.find(delayedJob => matchesJobId(delayedJob)),
 	};
 };
 
