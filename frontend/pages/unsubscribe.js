d9555b2bdf7fa8ca782fb48ac9b9979d8d119c77
k4m4 <nikolaskam@gmail.com>
2020-07-15 21:18:37 +0300

    Add re-subscription and account deletion buttons to /unsubscribe

--- frontend/pages/unsubscribe.js
+++ frontend/pages/unsubscribe.js
@@ -1,40 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Router from 'next/router';
 import PropTypes from 'prop-types';
 import fetch from 'isomorphic-unfetch';
 import { toast } from 'react-toastify';
+import NProgress from 'nprogress';
 import Layout from '../components/Layout';
 import Footer from '../components/Footer';
 import Main from '../components/Main';
 import SuccessIcon from '../components/SuccessIcon';
 import UnsuccessIcon from '../components/UnsuccessIcon';
+import AccountDeleted from '../components/AccountDeleted';
 import getHost from '../utils/get-host';
 import toastOptions from '../utils/toast';
+import {
+	clearCookies,
+	resubscribe,
+	deleteAccount,
+} from '../logic/preferences';
 
-export const Unsubscribe = ({ message, email }) => (
-	<Layout>
-		<Main>
-			{email ? (
-				<>
-					<SuccessIcon />
-					<h3><span className='courier bg-white black pa1'>{email}</span> successfully unsubscribed</h3>
-					<p>You will no longer be receiving Vanity emails</p>
-				</>
-			) : (
-				<>
-					<UnsuccessIcon />
-					<h3>unsubscription failed</h3>
-					<p>{message}</p>
-				</>
-			)}
-		</Main>
-		<Footer />
-	</Layout>
-);
+export const Unsubscribe = ({ message, email, token }) => {
+	const [accountDeleted, setAccountDeleted] = useState(false);
+
+	const handleResubscribe = () => resubscribe(token);
+	const handleDeleteAccount = async () => {
+		NProgress.start();
+
+		const deleted = await deleteAccount(token);
+		if (deleted) {
+			setAccountDeleted(true);
+			clearCookies();
+		}
+
+		NProgress.done();
+	};
+
+	if (accountDeleted) {
+		return <AccountDeleted token={token} />;
+	}
+
+	return (
+		<Layout>
+			<Main>
+				{email ? (
+					<>
+						<SuccessIcon />
+						<h3><span className='courier bg-white black pa1'>{email}</span> successfully unsubscribed</h3>
+						<p>You will no longer be receiving Vanity emails</p>
+						<span className='db'>
+							<a
+								className='link f5 ph3 pv2 ma2 dib white ba bw1 b--blue br2 bg-blue bg-animate hover-bg-transparent hover-blue'
+								onClick={handleResubscribe}
+							>
+								re-subscribe
+							</a>
+							<a
+								className='link f5 ph3 pv2 ma2 dib white ba bw1 b--red br2 bg-red bg-animate hover-bg-transparent hover-red'
+								onClick={handleDeleteAccount}
+							>
+								delete account
+							</a>
+						</span>
+					</>
+				) : (
+					<>
+						<UnsuccessIcon />
+						<h3>unsubscription failed</h3>
+						<p>{message}</p>
+					</>
+				)}
+			</Main>
+			<Footer />
+		</Layout>
+	);
+};
 
 Unsubscribe.propTypes = {
 	message: PropTypes.string,
 	email: PropTypes.string,
+	token: PropTypes.string.isRequired,
 };
 
 Unsubscribe.defaultProps = {
@@ -67,7 +110,7 @@ Unsubscribe.getInitialProps = async ({ req, res, query }) => {
 		const { error, user } = await response.json();
 
 		if (response.ok) {
-			return user;
+			return { ...user, token };
 		}
 
 		if (error) {
