890b5d67e8f0367b5c3719b9679f94b42e12f33c
k4m4 <nikolaskam@gmail.com>
2020-07-23 16:18:29 +0300

    Create 404 page tests

--- frontend/cypress/integration/index.spec.js
+++ frontend/cypress/integration/index.spec.js
@@ -81,3 +81,37 @@ describe('Unsubscribe page', () => {
 		cy.url().should('match', /\//);
 	});
 });
+
+describe('Not Found page', () => {
+	beforeEach(() => {
+		cy.visit('/404', { failOnStatusCode: false });
+	});
+
+	it('has head metadata', () => {
+		cy.get('head title').contains('Vanity');
+		cy.get('head meta[name="description"]').should('have.attr', 'content', 'Weekly metrics for your GitHub repos.');
+	});
+
+	it('has header navbar', () => {
+		cy.get('nav').within(() => {
+			cy.contains('a', 'Vanity').should('have.attr', 'href', '/');
+			cy.contains('a', 'Source').should('have.attr', 'href', 'https://github.com/meeshkan/vanity');
+			cy.get('i').contains('person').parent().should('have.attr', 'href', '/login');
+		});
+	});
+
+	it('has content', () => {
+		cy.get('main').within(() => {
+			cy.contains('h1', '404');
+			cy.contains('p', 'this page does not exist');
+		});
+	});
+
+	it('has footer', () => {
+		cy.get('footer').within(() => {
+			cy.contains('a', 'Twitter').should('have.attr', 'href', 'https://twitter.com/meeshkan');
+			cy.contains('a', 'GitHub').should('have.attr', 'href', 'https://github.com/meeshkan');
+			cy.contains('a', 'Meeshkan').should('have.attr', 'href', 'http://meeshkan.com/');
+		});
+	});
+});
