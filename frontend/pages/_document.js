fe170072ee127d0164d6077b31e8531a06a334a0
k4m4 <nikolaskam@gmail.com>
2020-07-23 23:21:31 +0300

    Remove [maximum-scale] and [user-scalable] attributes for accessibility

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -16,7 +16,7 @@ class VanityDoc extends Document {
 					<meta name='format-detection' content='telephone=no' />
 					<meta
 						name='viewport'
-						content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+						content='width=device-width, initial-scale=1.0'
 					/>
 					<link
 						href='https://fonts.googleapis.com/css?family=Space+Mono'
